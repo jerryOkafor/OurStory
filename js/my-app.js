3d68fca7197b8e31faef966a4853e9e1c9bcd06a
Don Potencio <hanksjerry_dedon@yahoo.com>
2016-05-19 08:45:30 -0700

    Adding Icons

--- js/my-app.js
+++ js/my-app.js
@@ -7,10 +7,10 @@ var $$ = Dom7;
 var ourStory;
 
 //Get the json config files
-$.getJSON( "app-config.json",function(data){
+$.getJSON("app-config.json", function (data) {
 
     })
-    .done(function( data ) {
+    .done(function (data) {
         ourStory = data;
 
     });
@@ -22,28 +22,23 @@ var mainView = myApp.addView('.view-main', {
 
 });
 
- /*---------------------
+/*---------------------
  Home page
---------------------- */
+ --------------------- */
 myApp.onPageInit('index', function (page) {
-  var mySwiper = myApp.swiper('.swiper-container', {
-    pagination:'.swiper-pagination',
-    paginationClickable: true,
-    autoplay: 2500,
-    autoplayDisableOnInteraction: false
-  });
+
 
 });
 /*---------------------
  About page
---------------------- */
+ --------------------- */
 myApp.onPageInit('about-us', function (page) {
-  var mySwiper = myApp.swiper('.swiper-container', {
-    pagination:'.swiper-pagination',
-    paginationClickable: true,
-    autoplay: 2500,
-    autoplayDisableOnInteraction: false
-  });
+    var mySwiper = myApp.swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        autoplay: 2500,
+        autoplayDisableOnInteraction: false
+    });
 
 
     //strat using jquery to bind the inner html
@@ -55,31 +50,25 @@ myApp.onPageInit('about-us', function (page) {
     //begin Grooms config
     $('#g-name').text(ourStory.ourStory.groom.name);
     $('#g-text').text(ourStory.ourStory.groom.about);
-    $('#g-f-link').attr('href',ourStory.ourStory.groom.facebookLink);
-    $('#g-t-link').attr('href',ourStory.ourStory.groom.twitterHandler);
-    $('#g-l-link').attr('href',ourStory.ourStory.groom.linkedinLink);
+    $('#g-f-link').attr('href', ourStory.ourStory.groom.facebookLink);
+    $('#g-t-link').attr('href', ourStory.ourStory.groom.twitterHandler);
+    $('#g-l-link').attr('href', ourStory.ourStory.groom.linkedinLink);
     //end grooms config
 
     //begin Bride's config
     $('#b-name').text(ourStory.ourStory.bride.name);
     $('#b-text').text(ourStory.ourStory.bride.about);
-    $('#b-f-link').attr('href',ourStory.ourStory.bride.facebookLink);
-    $('#b-t-link').attr('href',ourStory.ourStory.bride.twitterHandler);
-    $('#b-l-link').attr('href',ourStory.ourStory.bride.linkedinLink);
+    $('#b-f-link').attr('href', ourStory.ourStory.bride.facebookLink);
+    $('#b-t-link').attr('href', ourStory.ourStory.bride.twitterHandler);
+    $('#b-l-link').attr('href', ourStory.ourStory.bride.linkedinLink);
     //end bride's config
 
 
 });
- /*---------------------
+/*---------------------
  our story page
---------------------- */
+ --------------------- */
 myApp.onPageInit('our-story', function (page) {
-    var mySwiper = myApp.swiper('.swiper-container', {
-        pagination:'.swiper-pagination',
-        paginationClickable: true,
-        autoplay: 2500,
-        autoplayDisableOnInteraction: false
-    });
 
     $("#our-love-story").text(ourStory.ourStory.ourLoveStory);
     $("#first-met").text(ourStory.ourStory.firstMetStory);
@@ -94,13 +83,6 @@ myApp.onPageInit('our-story', function (page) {
  Gallery page
  --------------------- */
 myApp.onPageInit('gallery-page', function (page) {
-    var mySwiper = myApp.swiper('.swiper-container', {
-        pagination:'.swiper-pagination',
-        paginationClickable: true,
-        autoplay: 2500,
-        autoplayDisableOnInteraction: false
-    });
-
     //add click to all other links
     var l1 = $('#g-1');
     var l2 = $('#g-2');
@@ -114,8 +96,7 @@ myApp.onPageInit('gallery-page', function (page) {
 
     var setGallery = function (pos) {
 
-        switch (pos)
-        {
+        switch (pos) {
             case 0:
                 l1.addClass('active');
                 l2.removeClass('active');
@@ -142,34 +123,32 @@ myApp.onPageInit('gallery-page', function (page) {
                 break
         }
     };
-    $('#gallery-next').on('click',function(e){
-        if(pos < 5)
-        {
+    $('#gallery-next').on('click', function (e) {
+        if (pos < 5) {
             pos++;
         }
         setGallery(pos)
     });
 
     //set for the prevois button
-    $('#gallery-prev').on('click',function(e){
+    $('#gallery-prev').on('click', function (e) {
 
-        if(pos!=0){
+        if (pos != 0) {
             pos--;
         }
         setGallery(pos)
     });
 
 
-    g1.on('click',function(e){
+    g1.on('click', function (e) {
         pos = 0;
         l1.addClass('active');
         l2.removeClass('active');
         l3.removeClass('active');
         l4.removeClass('active');
-        myApp.alert(ourStory.ourStory.groom.name);
     });
 
-    g2.on('click',function(e){
+    g2.on('click', function (e) {
         pos = 1;
         l2.addClass('active');
         l1.removeClass('active');
@@ -177,7 +156,7 @@ myApp.onPageInit('gallery-page', function (page) {
         l4.removeClass('active');
     });
 
-    g3.on('click',function(e){
+    g3.on('click', function (e) {
         pos = 2;
         l3.addClass('active');
         l2.removeClass('active');
@@ -185,7 +164,7 @@ myApp.onPageInit('gallery-page', function (page) {
         l4.removeClass('active');
     });
 
-    g4.on('click',function(e){
+    g4.on('click', function (e) {
         pos = 3;
         l4.addClass('active');
         l2.removeClass('active');
@@ -196,13 +175,25 @@ myApp.onPageInit('gallery-page', function (page) {
 
 });
 
-(function ($) {
- "use strict";
 
-$(function(){
-$( '.swipebox' ).swipebox();
+/*---------------------
+ Invitation page
+ --------------------- */
+myApp.onPageInit('our-story', function (page) {
+    //myApp.alert(ourStory.ourStory.groom.name);
+    $('#invitaion-text-p1').text(ourStory.ourStory.invitation.p1);
+    $('#invitaion-text-p2').text(ourStory.ourStory.invitation.p2);
+    $('#g-name').text(ourStory.ourStory.groom.name);
+    $('#b-name').text(ourStory.ourStory.bride.name);
 
 });
+(function ($) {
+    "use strict";
+
+    $(function () {
+        $('.swipebox').swipebox();
+
+    });
 
 
 })(jQuery);
